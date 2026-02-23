08884903bcf88ad19e44164051c76826cb841ad6
aaron.pritzlaff <aaron.pritzlaff@gmail.com>
2026-02-23 12:51:49 +0000

    feat: add JukeboxFrame container component

---
+++ src/components/jukebox/JukeboxFrame.tsx
@@ -0,0 +1,51 @@
+// src/components/jukebox/JukeboxFrame.tsx
+import { ArchFrame } from "./ArchFrame";
+import { PanelFrame } from "./PanelFrame";
+import { BaseFrame } from "./BaseFrame";
+
+type Props = {
+  wurlitzer: React.ReactNode;
+  rolodex: React.ReactNode;
+  wizard: React.ReactNode;
+  queue?: React.ReactNode;
+};
+
+export function JukeboxFrame({ wurlitzer, rolodex, wizard, queue }: Props) {
+  return (
+    <div className="mx-auto w-full max-w-[500px]">
+      {/* Outer jukebox shell */}
+      <div className="rounded-t-[120px] bg-gradient-to-b from-amber-800 via-amber-900 to-neutral-900 p-3 shadow-[0_40px_100px_rgba(0,0,0,0.8)]">
+        {/* Top arch section - Wurlitzer */}
+        <ArchFrame className="h-[260px]">
+          {wurlitzer}
+        </ArchFrame>
+
+        {/* Middle section - Rolodex song picker */}
+        <div className="mt-3">
+          <PanelFrame className="min-h-[200px]">
+            {rolodex}
+          </PanelFrame>
+        </div>
+
+        {/* Bottom section - Sign-up wizard */}
+        <div className="mt-3">
+          <BaseFrame className="min-h-[120px]">
+            {wizard}
+          </BaseFrame>
+        </div>
+
+        {/* Optional queue display */}
+        {queue && (
+          <div className="mt-3">
+            <BaseFrame className="min-h-[100px]">
+              {queue}
+            </BaseFrame>
+          </div>
+        )}
+      </div>
+
+      {/* Jukebox base */}
+      <div className="h-8 rounded-b-xl bg-gradient-to-b from-neutral-800 to-neutral-950 shadow-lg" />
+    </div>
+  );
+}
