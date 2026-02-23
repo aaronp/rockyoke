6493391b17f96df312794daa62382cc2e4c0ac80
aaron.pritzlaff <aaron.pritzlaff@gmail.com>
2026-02-23 12:58:16 +0000

    feat: add jukebox component barrel export

---
+++ src/components/jukebox/index.ts
@@ -0,0 +1,9 @@
+// src/components/jukebox/index.ts
+export { JukeboxFrame } from "./JukeboxFrame";
+export { ArchFrame } from "./ArchFrame";
+export { PanelFrame } from "./PanelFrame";
+export { BaseFrame } from "./BaseFrame";
+export { Wurlitzer } from "./Wurlitzer";
+export { Rolodex } from "./Rolodex";
+export { SignUpWizard } from "./SignUpWizard";
+export { QueueDisplay } from "./QueueDisplay";
